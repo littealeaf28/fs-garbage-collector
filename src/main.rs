a56007ee064aae4b6c01e5e430ef4c666f12a0bc
Tianrui Li <tealeaf@icari.io>
2022-06-26 07:28:12 -0700

    Connects to and checks and pulls from redis

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,8 @@ use notify::{Watcher, RecursiveMode, watcher, DebouncedEvent};
 use std::sync::mpsc::channel;
 use std::time::Duration;
 
+use redis::Commands;
+
 fn main() {
     thread::spawn(|| {
         let html_content = "<html><body><h1>Hello, World!</h1></body></html>";
@@ -13,35 +15,42 @@ fn main() {
         web_view::builder()
             .title("FS Garbage Collector")
             .content(Content::Html(html_content))
-            .size(320, 480)
-            .resizable(false)
+            // .size(320, 480)
+            .resizable(true)
             .debug(true)
             .user_data(())
             .invoke_handler(|_webview, _arg| Ok(()))
             .run()
             .unwrap();
-    });
 
         // Webview should run and pull read file
         // Should have option to delete
+    });
 
     // Spawn another thread that listens for specific keyboard shortcut to pull up again
 
-    // Need config to know what listening for
-    // Loop with listening for changes
+    let client = redis::Client::open("redis://127.0.0.1/").unwrap();
+    let mut con = client.get_connection().unwrap();
 
-    // Scaling to multiple checked directories
+    // lpush
+    // ldelete
+
     let (tx, rx) = channel();
-    
     let mut watcher = watcher(tx, Duration::from_secs(10)).unwrap();
 
-    // TODO: How to read from .env file?
-    watcher.watch(check_dir, RecursiveMode::Recursive).unwrap();
-    watcher.watch(check_dir_two, RecursiveMode::Recursive).unwrap();
+    let check_dirs: Vec<String> = con.lrange("check_dirs", 0, -1).unwrap();
+    for dir in check_dirs.iter() {
+        watcher.watch(dir, RecursiveMode::Recursive).unwrap();
+    }
 
     loop {
         match rx.recv() {
-            Ok(DebouncedEvent::Create(path)) => println!("{}", path.display()),
+            Ok(DebouncedEvent::Create(path)) => {
+                println!("{}", path.into_os_string().into_string().unwrap())
+                // Add to tree
+                // TODO: Figure out how to use redis.json
+            },
+            // Ok(DebouncedEvent::R) - Delete
             Ok(_e) => println!("Other event"),
             Err(e) => println!("Error: {:?}", e),
         }
